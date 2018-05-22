8ae1340261cb128aefc5685724c683f9d304be06
Jason <jnelson2@gmail.com>
2018-05-22 12:44:26 -0400

    Clean code. Minor display changes. Finished Product de end exit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tic_tac_toe
 Simple Tic Tac Toe game, using ruby (non GUI)
+
+The game can be played either as single-player, against the computer, or as 2-player.  Constructing the TicTacToe object using two name parameters initiates a 2-person game, while passing only one name will begin a single-player game.
+
+The game is played via the console/terminal interface.  There is no GUI interface at this point.  
+
+For simplicity, the computer does not intelligently choose its squares.  It merely chooses a square at random among the remaining choices.  
+
+The game results in either player winning, or a tie.
+
+ 
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,8 @@
 
+# The Tic_Tac_Toe game squares
 class Square
-  attr_accessor :position, :selected, :owner
+  attr_reader :position
+  attr_accessor :selected, :owner
 
   def initialize(number)
     @position = number
@@ -15,30 +17,28 @@ class Square
 
 end
 
-
+# A player in the game
 class Player
-  attr_reader :my_squares, :name
-  attr_accessor :play_odd_rounds, :winner
+  attr_reader :name
+  attr_accessor :winner
 
   def initialize(name)
     @name = name
     @winner = false
-    @my_squares = []
   end
 
 end
 
+# Player operated by a human user
 class User < Player
 
   def choose_square(squares, available_square_positions)
     all_positions = (1..9).to_a
-    puts "Options:"
-    puts " "
-    puts "( 7 | 8 | 9 )"
-    puts "(--- --- ---)"
-    puts "( 4 | 5 | 6 )"
-    puts "(--- --- ---)"
-    puts "( 1 | 2 | 3 )"
+    puts "Options:    ( 7 | 8 | 9 )"
+    puts "            (--- --- ---)"
+    puts "            ( 4 | 5 | 6 )"
+    puts "            (--- --- ---)"
+    puts "            ( 1 | 2 | 3 )"
     puts " "
     begin
       puts "#{@name}, please choose a square (1 through 9):"
@@ -54,36 +54,26 @@ class User < Player
       retry
     end
     chosen_square = squares.find { |square| square.position == choice }
-    puts "chosen_square is class: " + chosen_square.class.to_s
     chosen_square.mark_selected(@name)
-    puts "Chose square " + chosen_square.position.to_s
-    puts "chosen square owner = " + chosen_square.owner.to_s
-    puts "chosen square selected? = " + chosen_square.selected.to_s
-    puts "end of choose_square"
-    puts " "
     puts "#{@name} has chosen square #{choice}."
   end
 
 end
 
+# A Player automated by the computer
 class Computer < Player
 
   def choose_square(squares, available_square_positions)
     choice = available_square_positions.shuffle[0]
     chosen_square = squares.find { |square| square.position == choice }
     chosen_square.mark_selected(@name)
-    puts "Chose square " + chosen_square.position.to_s
-    puts "chosen square owner = " + chosen_square.owner.to_s
-    puts "chosen square selected? = " + chosen_square.selected.to_s
-    puts "end of choose_square"
-    puts " "
     puts "#{@name} has chosen square #{choice}."
   end
 
 end
 
-class Game
-  attr_accessor :selected_squares
+# The main game mechanisim
+class TicTacToe
 
   WINNING_COMBOS = [  [7, 8, 9],
                       [4, 5, 6],
@@ -96,74 +86,70 @@ class Game
 
   def initialize(player1, player2 = "NotSelected")
     @player1 = User.new(player1)
+    # player2 is assigned as computer if no second name is passed
     if player2 == "NotSelected"
       @player2 = Computer.new("Computer")
     else
       @player2 = User.new(player2)
     end
     @squares = (1..9).map { |num| Square.new(num) }
-    @selected_squares = []
     @game_won = false
   end
 
+  # Randomly assigns the first turn
   def set_first_turn
     num = rand(2)
     num < 1 ? @player1 : @player2
   end
 
+  # Initiates the game
   def play_game
     odd_player = set_first_turn
     even_player = (odd_player == @player1) ? @player2 : @player1
     round = 1
-    until (@game_won == true || round > 9)
+    # initialize the on-going game display
+    puts " "
+    puts "============================================"
+    puts " "
+    until (@game_won == true || get_available_square_positions.empty?)
       current_player = round.odd? ? odd_player : even_player
       current_player.choose_square(@squares, get_available_square_positions)
       @game_won = true if check_winner_exists
       update_game_display
       round += 1
     end
-    if round > 9
-      puts "Tie Game! Nobody wins :( "
-    else
+    if @game_won == true
       show_winner
+    else
+      puts "Tie Game! Nobody wins :( "
     end
   end
 
+  # Returns the positions (1-9) currently available to players
   def get_available_square_positions
     available_squares = @squares.select { |square| square.selected == false }
     available_squares.map { |square| square.position }
   end
 
+  # Returns the positions (1-9) currently owned by a player
   def get_player_square_positions(player_name)
     player_squares = @squares.select { |square| square.owner == player_name }
     player_squares.map { |square| square.position }
   end
 
-
+  # Displays the current game board
   def update_game_display
     marks  = Hash[@squares.collect { |item| [item.position, item.owner] } ]
     marks.each do  |position, owner|
-      puts "display: position = " + position.to_s
-      puts "display: owner = " + owner.to_s
       case owner
       when @player1.name
-        # owner = "X"
         marks[position] = "X"
-        puts "assigned X for P1"
       when @player2.name
-        # owner = "O"
         marks[position] = "O"
-        puts "assigned O for P2"
       else
-        # owner = " "
         marks[position] = " "
-        puts "assigned 'space' to nobody"
       end
-      puts "End of display EACH block"
     end
-
-
-
 
     puts " "
     puts " #{marks[7]} | #{marks[8]} | #{marks[9]} "
@@ -172,19 +158,16 @@ class Game
     puts "--- --- ---"
     puts " #{marks[1]} | #{marks[2]} | #{marks[3]} "
     puts " "
-    puts "==========================="
+    puts "============================================"
     puts " "
   end
 
+  # Returns true if there is a winner, and updates the winning player object
   def check_winner_exists
     require 'set'
     has_winner = false
     player1_positions = get_player_square_positions(@player1.name)
-    puts "p1 current positions:"
-    p player1_positions
     player2_positions = get_player_square_positions(@player2.name)
-    puts "p2 current positions:"
-    p player2_positions
     WINNING_COMBOS.each do |combo|
       if combo.to_set.subset?(player1_positions.to_set)
         @player1.winner = true
@@ -197,6 +180,7 @@ class Game
     return has_winner
   end
 
+  # Displays the winner
   def show_winner
     if @player1.winner == true
       puts "#{@player1.name} Wins!!"
@@ -209,5 +193,10 @@ class Game
 
 end
 
-play = Game.new("Betty")
-play.play_game
+# Play single-player game, one user against the computer
+game = TicTacToe.new("Jason")
+game.play_game
+
+# Play 2-player game, with two users
+# game = TicTacToe.new("Jason", "Betty")
+# game.play_game

cfae304bab6d0872813e4156c40b0cb89572b430
Jason <30456017+jn4248@users.noreply.github.com>
2018-09-03 20:21:21 -0700

    Create tic_tac_toe_test.rb to run game testing
    Remove test code from tic_tac_toe.rb

---
+++ tic_tac_toe_test.rb
@@ -0,0 +1,9 @@
+require "tic_tac_toe"
+
+# Play single-player game, one user against the computer
+game = TicTacToe.new("Jason")
+game.play_game
+
+# Play 2-player game, with two users
+# game = TicTacToe.new("Jason", "Betty")
+# game.play_game
